f9f882642d408abba70f6f1660dcf005bcb58e13
Alejandro <asber_dhc@hotmail.es>
2020-02-18 16:13:53 -0600

    Deleting and recreating the ProductDTO class

--- Products/Models/ProductDTO.cs
+++ Products/Models/ProductDTO.cs
@@ -7,9 +7,9 @@ namespace Products.Models
 {
     public class ProductDTO
     {
-        public int  IdProduct { get; set; }
+        public int IdProduct { get; set; }
         public string Name { get; set; }
-        public string  Description { get; set; }
+        public string Description { get; set; }
         public decimal Price { get; set; }
         public byte[] Image { get; set; }
     }
