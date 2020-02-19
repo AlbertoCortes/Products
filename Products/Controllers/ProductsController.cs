2dac73559961c1ed3f1404e1eeef922049e4f14d
Alejandro <asber_dhc@hotmail.es>
2020-02-19 13:50:35 -0600

    Refactoring Update(int id, [FromBody]ProductDTO prod)

--- Products/Controllers/ProductsController.cs
+++ Products/Controllers/ProductsController.cs
@@ -135,20 +135,38 @@ namespace Products.Controllers
         //PUT api/Products/5
         [HttpPut]
         [ResponseType(typeof(ProductDTO))]
-        public IHttpActionResult Update([FromBody]ProductDTO prod)
+        public IHttpActionResult Update(int id, [FromBody]ProductDTO prod)
         {
             try
             {
-                var prodmodif = db.Products.FirstOrDefault(p => p.Id == prod.IdProduct);
-                prodmodif.Nombre = prod.Name;
-                prodmodif.Description = prod.Description;
-                prodmodif.PriceClient = prod.Price;
+                //verify that the user does not include the id within json file
+                if (prod.IdProduct != 0)
+                    return BadRequest("The id should be included only in the URL");
+
+                //updating the product info
+                var prodModified = db.Products.Where(p => p.Id == id && p.IsEnabled == true).FirstOrDefault();
+                if (prodModified == null)
+                    return StatusCode(HttpStatusCode.NotFound);
+                prodModified.Nombre = prod.Name;
+                prodModified.Description = prod.Description;
+                prodModified.PriceClient = prod.Price;
+
+                //updating the product image
+                if (prod.Image != null)
+                {
+                    var image = db.ImagesProduct.FirstOrDefault(i => i.IdImageProduct == id);
+                    image.Image = prod.Image;
+                }
+
                 db.SaveChanges();
+
+                //return the updated ProductDTO
+                prod.IdProduct = id;
                 return Ok(prod);
             }
             catch (Exception)
             {
-                return BadRequest("Product do not match with any in DB");
+                return StatusCode(HttpStatusCode.InternalServerError);
             }
         }
 
@@ -159,12 +177,18 @@ namespace Products.Controllers
             try
             {
                 var productOnDB = db.Products.FirstOrDefault(p => p.Id == id);
+
+                //validate that the product exist
+                if (productOnDB == null || productOnDB.IsEnabled == false)
+                    return StatusCode(HttpStatusCode.NotFound);
+
+                //deleting the product and it's images
                 productOnDB.IsEnabled = false;
                 var images = db.ImagesProduct.Where(i => i.IdImageProduct == id);
                 foreach (ImagesProduct image in images)
                     image.IsEnabled = "0";
-                db.SaveChanges();
 
+                db.SaveChanges();
                 return Ok();
             }
             catch (Exception)
